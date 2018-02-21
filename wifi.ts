15837d6237ff30e67c0070a5459983c64c0a0ed8
Ken Lai <ken.lai@muselab.cc>
2018-02-21 16:19:51 +0800

    test the gap

--- wifi.ts
+++ wifi.ts
@@ -51,6 +51,7 @@ namespace MuseRobotic {
     //% block="Turn %motor| on direction %dir|speed %speed"
     //% speed.min=0 speed.max=100
 	//% weight=70	
+	//% blockGap=4
     export function motorOn(motor: Motors, dir: MotorDirection, speed: number): void {
 		
         let OutputVal = Math.clamp(0, 100, speed) * 10;
